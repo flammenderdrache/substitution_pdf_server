3f4cd69ca905c98af17b0ca588bff6340883aaec
flamion <laberschleichi@gmail.com>
2022-01-21 12:11:25 +0100

    Add files

---
+++ src/main.rs
@@ -0,0 +1,215 @@
+use std::collections::HashMap;
+use std::fmt::{Display, Formatter};
+use std::io::Write;
+use std::path::Path;
+use std::sync::Arc;
+use std::time::Duration;
+
+use actix_web::{App, HttpServer, web};
+use chrono::{Datelike, DateTime, Local, Weekday};
+use lazy_static::lazy_static;
+use reqwest::Client;
+use tokio::sync::RwLock;
+use tracing::{debug, error, info, trace};
+
+use substitution_pdf_to_json::SubstitutionSchedule;
+
+mod util;
+
+const TEMP_ROOT_DIR: &str = "/tmp/school-substitution-scanner-temp-dir";
+const SOURCE_URLS: [&str; 5] = [
+	"https://buessing.schule/plaene/VertretungsplanA4_Montag.pdf",
+	"https://buessing.schule/plaene/VertretungsplanA4_Dienstag.pdf",
+	"https://buessing.schule/plaene/VertretungsplanA4_Mittwoch.pdf",
+	"https://buessing.schule/plaene/VertretungsplanA4_Donnerstag.pdf",
+	"https://buessing.schule/plaene/VertretungsplanA4_Freitag.pdf",
+];
+const PDF_GET_LOOP_SLEEP_TIME: Duration = Duration::from_secs(20);
+
+lazy_static! {
+	static ref PDF_JSON_STORE: RwLock<HashMap<Schoolday, String>> = RwLock::new(HashMap::new());
+}
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+	tracing_subscriber::fmt::init();
+
+	// Make sure the temp path exists
+	std::fs::create_dir_all(TEMP_ROOT_DIR)?;
+
+	tokio::spawn(async {
+		let pdf_getter = Arc::new(SubstitutionPDFGetter::default());
+		let mut counter: u32 = 0;
+
+		info!("Starting loop!");
+		loop {
+			trace!("loop started");
+
+			let local: DateTime<Local> = Local::now();
+			let next_valid_school_weekday = Schoolday::from(local.weekday());
+			let day_after = next_valid_school_weekday.next_day();
+
+			debug!("Local day: {}; next valid school day: {}; day after that: {}",
+			local.weekday(),
+			next_valid_school_weekday,
+			day_after
+		);
+
+			let pdf_getter_arc = pdf_getter.clone();
+			if let Err(why) = check_weekday_pdf(next_valid_school_weekday, pdf_getter_arc).await {
+				error!("{}", why);
+			}
+
+			let pdf_getter_arc = pdf_getter.clone();
+			tokio::spawn(async move {
+				if let Err(why) = check_weekday_pdf(day_after, pdf_getter_arc).await {
+					error!("{}", why);
+				}
+			});
+
+			counter += 1;
+			debug!("Loop ran {counter} times and fetched {} PDFs", counter * 2);
+			trace!("Loop end before sleep");
+			tokio::time::sleep(PDF_GET_LOOP_SLEEP_TIME).await;
+		}
+	});
+
+	tokio::spawn(async {
+		HttpServer::new(move || {
+			// let json_config = web::JsonConfig::default()
+			// 	.limit(4096);
+
+			App::new()
+				.service(web::scope("/user")
+					.service(create_user)
+					.service(get_user)
+					.service(login)
+				)
+		})
+			.bind("127.0.0.1:8080")?
+			.run()
+			.await?;
+	});
+
+	Ok(())
+}
+
+/// Downloads the pdf of the current weekday, converts it to a json and adds it to the map of jsons.
+#[allow(clippy::or_fun_call)]
+async fn check_weekday_pdf(day: Schoolday, pdf_getter: Arc<SubstitutionPDFGetter<'_>>) -> Result<(), Box<dyn std::error::Error>> {
+	info!("Checking PDF for {}", day);
+	let temp_dir_path = util::make_temp_dir();
+	let temp_file_name = util::get_random_name();
+	let temp_file_path = format!("{}/{}", temp_dir_path, temp_file_name);
+	let temp_file_path = Path::new(&temp_file_path);
+
+	let pdf = pdf_getter.get_weekday_pdf(day).await?;
+	let mut temp_pdf_file = std::fs::File::create(temp_file_path).expect("Couldn't create temp pdf file");
+	temp_pdf_file.write_all(&pdf)?;
+
+	let new_schedule = SubstitutionSchedule::from_pdf(temp_file_path)?;
+	let schedule_json = serde_json::to_string_pretty(&new_schedule).expect("Couldn't write the new Json");
+
+	{
+		let mut jsons = PDF_JSON_STORE.write().await;
+		let _ = jsons.insert(day, schedule_json);
+	}
+
+	std::fs::remove_file(temp_file_path)?;
+	std::fs::remove_dir(temp_dir_path)?;
+
+
+	Ok(())
+}
+
+/// Enum with the weekdays where a Substitution PDF is available.
+#[derive(Debug, PartialOrd, PartialEq, Clone, Copy, Hash, Eq)]
+pub enum Schoolday {
+	Monday = 0,
+	Tuesday = 1,
+	Wednesday = 2,
+	Thursday = 3,
+	Friday = 4,
+}
+
+impl Schoolday {
+	//It is not &self, just self here due to https://rust-lang.github.io/rust-clippy/master/index.html#trivially_copy_pass_by_ref
+	//Thank clippy :p
+	pub fn next_day(self) -> Self {
+		match self {
+			Schoolday::Monday => Schoolday::Tuesday,
+			Schoolday::Tuesday => Schoolday::Wednesday,
+			Schoolday::Wednesday => Schoolday::Thursday,
+			Schoolday::Thursday => Schoolday::Friday,
+			Schoolday::Friday => Schoolday::Monday,
+		}
+	}
+}
+
+impl Display for Schoolday {
+	fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+		let self_as_string = match self {
+			Schoolday::Monday => "Monday",
+			Schoolday::Tuesday => "Tuesday",
+			Schoolday::Wednesday => "Wednesday",
+			Schoolday::Thursday => "Thursday",
+			Schoolday::Friday => "Friday",
+		};
+
+		write!(f, "{}", self_as_string)
+	}
+}
+
+impl From<Weekday> for Schoolday {
+	fn from(day: Weekday) -> Self {
+		match day {
+			Weekday::Tue => Schoolday::Tuesday,
+			Weekday::Wed => Schoolday::Wednesday,
+			Weekday::Thu => Schoolday::Thursday,
+			Weekday::Fri => Schoolday::Friday,
+			_ => Schoolday::Monday,
+		}
+	}
+}
+
+pub struct SubstitutionPDFGetter<'a> {
+	urls: [&'a str; 5],
+	client: Client,
+}
+
+impl<'a> SubstitutionPDFGetter<'a> {
+	pub fn new(client: Client) -> Self {
+		Self {
+			urls: SOURCE_URLS,
+			client,
+		}
+	}
+
+	/// Returns result with an Err or a Vector with the binary data of the request-response
+	/// Does not check if the response is valid, this is the responsibility of the caller.
+	pub async fn get_weekday_pdf(&self, day: Schoolday) -> Result<Vec<u8>, reqwest::Error> {
+		let url = self.urls[day as usize];
+		let request = self.client
+			.get(url)
+			.header("Authorization", "Basic aGJzdXNlcjpoYnNwYXNz")
+			.build()
+			.unwrap();
+
+		let response = self.client.execute(request).await?;
+		let bytes = response.bytes().await?;
+
+		Ok(bytes.to_vec())
+	}
+}
+
+impl<'a> Default for SubstitutionPDFGetter<'a> {
+	fn default() -> Self {
+		let client = Client::builder()
+			.connect_timeout(Duration::from_secs(20))
+			.timeout(Duration::from_secs(20))
+			.build()
+			.unwrap();
+
+		Self::new(client)
+	}
+}
---
+++ substitution_pdf_to_json/src/lib.rs
@@ -0,0 +1,284 @@
+use std::collections::HashMap;
+use std::ffi::OsStr;
+use std::fmt::{Display, Formatter};
+use std::path::Path;
+use std::process::Command;
+use std::str;
+use std::time::SystemTime;
+use thiserror::Error;
+
+use chrono::{Local, NaiveDate, Offset, Utc};
+use lopdf::Document;
+use serde::{Deserialize, Serialize};
+use serde_json::Value;
+
+/// One column with Substitutions from the PDF
+#[derive(Serialize, Deserialize, PartialOrd, PartialEq, Debug)]
+pub struct SubstitutionColumn {
+	#[serde(rename(serialize = "0"))]
+	#[serde(rename(deserialize = "0"))]
+	#[serde(skip_serializing_if = "Option::is_none")]
+	pub block_0: Option<String>,
+	#[serde(rename(serialize = "1"))]
+	#[serde(rename(deserialize = "1"))]
+	#[serde(skip_serializing_if = "Option::is_none")]
+	pub block_1: Option<String>,
+	#[serde(rename(serialize = "2"))]
+	#[serde(rename(deserialize = "2"))]
+	#[serde(skip_serializing_if = "Option::is_none")]
+	pub block_2: Option<String>,
+	#[serde(rename(serialize = "3"))]
+	#[serde(rename(deserialize = "3"))]
+	#[serde(skip_serializing_if = "Option::is_none")]
+	pub block_3: Option<String>,
+	#[serde(rename(serialize = "4"))]
+	#[serde(rename(deserialize = "4"))]
+	#[serde(skip_serializing_if = "Option::is_none")]
+	pub block_4: Option<String>,
+	#[serde(rename(serialize = "5"))]
+	#[serde(rename(deserialize = "5"))]
+	#[serde(skip_serializing_if = "Option::is_none")]
+	pub block_5: Option<String>,
+}
+
+/// Represents a column from the substitution PDF.
+/// Does not include the class name, only the substitutions.
+impl SubstitutionColumn {
+	pub fn new() -> Self {
+		Self {
+			block_0: None,
+			block_1: None,
+			block_2: None,
+			block_3: None,
+			block_4: None,
+			block_5: None,
+		}
+	}
+}
+
+impl Default for SubstitutionColumn {
+	fn default() -> Self {
+		Self::new()
+	}
+}
+
+impl Display for SubstitutionColumn {
+	fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+		write!(f, "{}", serde_json::to_string_pretty(self).unwrap())
+	}
+}
+
+/// Contains the extracted PDF data of the schedule PDF
+#[derive(Serialize, Deserialize, Debug)]
+pub struct SubstitutionSchedule {
+	/// The creation date inside the PDF
+	pub pdf_create_date: i64,
+	/// The name of the class is the Key and the Value is a Substitutions struct
+	entries: HashMap<String, SubstitutionColumn>,
+	/// The time when the struct was created, used for comparing the age
+	struct_time: u64,
+}
+
+impl SubstitutionSchedule {
+	/// Constructs an instance of `Self` from a document saved on disk.
+	pub fn from_pdf<T: AsRef<Path> + AsRef<OsStr>>(path: T) -> Result<Self, Box<dyn std::error::Error>> {
+		let pdf = match Document::load(&path) {
+			Ok(pdf) => pdf,
+			Err(_) => return Err(Box::new(PDFJsonError::PDFReadError)),
+		};
+
+		let page_numbers = get_all_page_numbers(&pdf);
+		let pdf = pdf.extract_text(&*page_numbers)?;
+
+		let date_idx_start = pdf.find("Datum: ").ok_or("date not found")?;
+		let date_idx_end = pdf[date_idx_start..].find('\n').ok_or("date end not found")? + date_idx_start;
+
+		let date_str: Vec<u32> = pdf[date_idx_start..date_idx_end].split(", ")
+			.last()
+			.ok_or("date string has no ','")?
+			.split('.')
+			.collect::<Vec<&str>>()
+			.iter()
+			.map(|s| (*s).parse::<u32>().unwrap())
+			.collect();
+
+		#[allow(clippy::cast_possible_wrap)]
+			let date = chrono::Date::<Local>::from_utc(
+			NaiveDate::from_ymd(date_str[2] as i32, date_str[1], date_str[0]),
+			Utc.fix(),
+		).and_hms_milli(0, 0, 0, 0).timestamp_millis();
+
+
+		let output = Command::new("java")
+			.arg("-jar")
+			.arg("./tabula/tabula.jar")
+			.arg("-g")
+			.arg("-f")
+			.arg("JSON")
+			.arg("-p")
+			.arg("all")
+			.arg(path)
+			.output()?;
+
+		let table = parse_tabula_json(str::from_utf8(&output.stdout).unwrap())?;
+
+		Ok(Self::from_table(&table, date))
+	}
+
+	/// Constructs an instance of `Self` from a table.
+	#[allow(clippy::ptr_arg)]
+	pub fn from_table(tables: &Vec<Vec<Vec<String>>>, pdf_create_date: i64) -> Self {
+		let mut entries = HashMap::new();
+
+		for table in tables {
+			entries.extend(Self::table_to_substitutions(table));
+		}
+
+		let time_now = SystemTime::now();
+		let since_the_epoch = time_now
+			.duration_since(SystemTime::UNIX_EPOCH)
+			.expect("Time got fucked");
+
+		#[allow(clippy::cast_possible_truncation)]
+			let time_millis = since_the_epoch.as_millis() as u64;
+
+		Self {
+			pdf_create_date,
+			entries,
+			struct_time: time_millis,
+		}
+	}
+
+	/// Grabs the classes and their substitutions from a table and turns them into a HashMap.
+	#[allow(clippy::ptr_arg)]
+	fn table_to_substitutions(table: &Vec<Vec<String>>) -> HashMap<String, SubstitutionColumn> {
+		let mut entries: HashMap<String, SubstitutionColumn> = HashMap::new();
+
+		let classes = &table[0][1..];
+
+		for class in classes {
+			entries.insert(class.to_string(), SubstitutionColumn::new());
+		}
+
+		let mut row = 1;
+
+		for lesson_idx in 0..5 {
+			loop {
+				for (i, substitution_part) in table[row][1..].iter().enumerate() {
+					let substitutions = entries.get_mut(&classes[i]).unwrap();
+
+					let block_option = match lesson_idx {
+						0 => &mut substitutions.block_0,
+						1 => &mut substitutions.block_1,
+						2 => &mut substitutions.block_2,
+						3 => &mut substitutions.block_3,
+						4 => &mut substitutions.block_4,
+						5 => &mut substitutions.block_5,
+						_ => panic!("more then 5 lessons used"),
+					};
+
+					if !substitution_part.is_empty() {
+						if let Some(block) = block_option {
+							block.push_str(&format!("\n{}", substitution_part.clone()));
+						} else {
+							let _ = block_option.insert(substitution_part.clone());
+						}
+					}
+				}
+
+				if table[row][0].starts_with('-') {
+					break;
+				}
+				row += 1;
+			}
+
+			row += 1;
+		}
+
+		entries
+	}
+}
+
+/// Gets all pages from the pdf document.
+fn get_all_page_numbers(pdf: &Document) -> Box<[u32]> {
+	let pages = pdf
+		.get_pages()
+		.keys()
+		.map(|page| *page)
+		.collect::<Box<[u32]>>();
+
+	pages
+}
+
+/// Extracts the text from the rows and cells in the json that gets outputted by tabula.
+pub fn parse_tabula_json(content: &str) -> Result<Vec<Vec<Vec<String>>>, Box<dyn std::error::Error>> {
+	let json: Value = serde_json::from_str(content)?;
+	let array = json.as_array().ok_or("Json malformed")?;
+
+	let mut tables = Vec::new();
+	for entry in array {
+		let object = entry.as_object().ok_or("Json malformed")?;
+		let data = object.get("data").ok_or("Json data field missing")?;
+
+		let mut table_rows = Vec::new();
+		for row in data.as_array().ok_or("Json data missing")? {
+			let row: Vec<Cell> = serde_json::from_value(row.clone())?;
+			let row = Row {
+				row
+			};
+			table_rows.push(row);
+		}
+		tables.push(table_rows);
+	}
+
+	let mut tables_with_rows_as_text = Vec::new();
+	for table_rows in tables {
+		let mut rows_as_text = Vec::new();
+		for mut row in table_rows {
+			rows_as_text.push(row.extract_text());
+		}
+		tables_with_rows_as_text.push(rows_as_text);
+	}
+
+	Ok(tables_with_rows_as_text)
+}
+
+/// A row in the substitution table
+#[derive(Debug, Deserialize, Serialize)]
+struct Row {
+	row: Vec<Cell>,
+}
+
+impl Row {
+	/// Gets the string content of every Cell inside the Row
+	pub fn extract_text(&mut self) -> Vec<String> {
+		let mut text = Vec::new();
+		for cell in &self.row {
+			text.push(cell.text.clone());
+		}
+
+		text
+	}
+}
+
+/// A cell in the substitution table
+#[derive(Debug, Deserialize, Serialize)]
+struct Cell {
+	top: f64,
+	left: f64,
+	width: f64,
+	height: f64,
+	text: String,
+}
+
+impl Display for Cell {
+	fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+		write!(f, "{}", self.text)
+	}
+}
+
+#[derive(Error, Debug)]
+pub enum PDFJsonError {
+	#[error("There was an error while reading the PDF File.")]
+	PDFReadError
+}
