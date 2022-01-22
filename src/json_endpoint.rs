ea2648f0c2200238390c5f04af9913ee92dfe5ae
flamion <laberschleichi@gmail.com>
2022-01-22 17:37:00 +0100

    Now replying with 204 NO_CONTENT if a json is not in storage

--- src/json_endpoint.rs
+++ src/json_endpoint.rs
@@ -6,12 +6,12 @@ use crate::PDF_JSON_STORE;
 pub async fn get_schoolday_pdf_json(day: web::Path<Schoolday>) -> impl Responder {
 	let store = PDF_JSON_STORE.read().await;
 	if let Some(json) = store.get(&day) {
-		HttpResponse::Ok()
+		return HttpResponse::Ok()
 			.content_type("application/json")
-			.body(json.clone())
-	} else {
-		HttpResponse::ServiceUnavailable()
-			.append_header(("Retry-After", "1"))
-			.finish()
+			.body(json.clone());
 	}
+
+	HttpResponse::NoContent()
+		.append_header(("Retry-After", "120"))
+		.finish()
 }
